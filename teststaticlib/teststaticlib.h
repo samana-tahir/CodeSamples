5044a9ac2ef1258c849bb680dcddfaacf79e4420
mi-app <app@massiveinfinity.com>
2018-08-20 03:55:26 +0800

    Initial Commit

---
+++ teststaticlib/teststaticlib.h
@@ -0,0 +1,13 @@
+//
+//  teststaticlib.h
+//  teststaticlib
+//
+//  Created by Samana Tahir on 20/08/2018.
+//  Copyright © 2018 Samana Tahir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface teststaticlib : NSObject
+
+@end
---
+++ teststaticlib/teststaticlib.m
@@ -0,0 +1,13 @@
+//
+//  teststaticlib.m
+//  teststaticlib
+//
+//  Created by Samana Tahir on 20/08/2018.
+//  Copyright © 2018 Samana Tahir. All rights reserved.
+//
+
+#import "teststaticlib.h"
+
+@implementation teststaticlib
+
+@end
